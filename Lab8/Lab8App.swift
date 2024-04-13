550c654333a7d3dce4045cf670e916fc67497752
Zachary <zach.lee96@gmai.com>
2024-04-13 03:49:04 -0700

    Initial Commit

---
+++ Lab8/Lab8App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab8App.swift
+//  Lab8
+//
+//  Created by Zachary Lee on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
